6ba2ec4d04315b2074a1dc91156650553dde3a5e
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-13 14:17:09 +0300

    Created command for migrations

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
 from app import create_app, db
 from flask_script import Manager, Server
-
+from app.models import User, Blog, Comment
+from flask_migrate import Migrate, MigrateCommand
 
 app=create_app('development')
 
 manager= Manager(app)
 manager.add_command('server', Server)
+migrate=Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
@@ -16,7 +19,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, User=User, Blog=Blog, Comment=Comment)
 
 
 if __name__ == '__main__':
