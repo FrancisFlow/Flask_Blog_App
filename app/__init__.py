ad22f91272a5196ba4a8e5f5e4a8a38f89dc0f44
FrancisFlow <iamngigikaranja1@aol.com>
2022-03-13 09:44:25 +0300

    Initialized loginManager for session management

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,34 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from config import config_options
+from flask_login import LoginManager
+from flask_mail import Mail
 
 
 
 #instantiating extentions
 bootstrap=Bootstrap()
 db=SQLAlchemy()
+mail=Mail()
 
-def create_app():
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
+
+def create_app(config_name):
 
     app=Flask(__name__)
 
-
+    #app configurations
+    app.config.from_object(config_options[config_name])
     #initializing flask extentions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    #Registering main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
